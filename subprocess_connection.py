a20fff3481eff55189a3311b2237df5c81baccc3
user202729 <25191436+user202729@users.noreply.github.com>
2021-06-11 19:16:27 +0700

    Add  class

--- subprocess_connection.py
+++ subprocess_connection.py
@@ -1,9 +1,10 @@
 import sys
 import io
-from typing import Optional, Any, BinaryIO
-from threading import Lock
+from typing import Optional, Any, BinaryIO, Callable, Dict, Union
+from threading import Lock, Thread
 from subprocess import Popen
 import pickle
+from queue import Queue
 
 class Connection:
 	"""
@@ -99,4 +100,147 @@ class Connection:
 		raise NotImplementedError
 
 
+_FUNCTION: str="_FUNCTION"
+_FUNCTION_RESPONSE: str="_FUNCTION_RESPONSE"
 
+class Message:
+	def __init__(self, x: Union[Connection, Popen]=None)->None:
+		if x is None:
+			connection=Connection()
+		elif isinstance(x, Connection):
+			connection=x
+		else:
+			connection=Connection(x)
+		self._connection: Connection=connection
+		self._calls: Dict[Any, Callable]={}
+		self._funcs: Dict[Any, Callable]={}
+
+		message: Message=self
+
+		class _CallProxy:
+			def __setitem__(self, key: Any, value: Callable)->None:
+				message.set_call(key, value)
+
+			def __setattr__(self, key: Any, value: Callable)->None:
+				message.set_call(key, value)
+
+			def __getitem__(self, key: Any)->Callable:
+				return lambda *args, **kwargs: message.call_remote(key, args, kwargs)
+
+			def __getattr__(self, key: Any)->Callable:
+				return lambda *args, **kwargs: message.call_remote(key, args, kwargs)
+
+
+		class _FuncProxy:
+			def __setitem__(self, key: Any, value: Callable)->None:
+				message.set_func(key, value)
+
+			def __setattr__(self, key: Any, value: Callable)->None:
+				message.set_func(key, value)
+
+			def __getitem__(self, key: Any)->Callable:
+				return lambda *args, **kwargs: message.func_remote(key, args, kwargs)
+
+			def __getattr__(self, key: Any)->Callable:
+				return lambda *args, **kwargs: message.func_remote(key, args, kwargs)
+
+		self._call_proxy=_CallProxy()
+		self._func_proxy=_FuncProxy()
+		self._func_response_queues: Dict[int, Queue]={}
+		self._response_counter: int=0
+		self._response_counter_lock: Lock=Lock()
+
+		self._exec_running: bool=False
+		self._exec_running_lock: Lock=Lock()
+
+		self._exec_thread: Thread=Thread(target=self.exec_)
+
+		self.call[_FUNCTION]=self._on_func_called
+		self.call[_FUNCTION_RESPONSE]=self._on_func_response
+
+	def set_call(self, key: Any, value: Callable)->None:
+		self._calls[key]=value
+
+	def set_func(self, key: Any, value: Callable)->None:
+		self._funcs[key]=value
+
+	def exec_(self, suppress_call_errors: bool=True)->None:
+		"""
+		Listen for and execute requests.
+		Usually this is run in a separate thread, but it's not required.
+		"""
+		with self._exec_running_lock:
+			if self._exec_running:
+				raise RuntimeError("exec_ is running")
+			self._exec_running=True
+		
+		try:
+			while True:
+				try:
+					data=self._connection.recv()
+				except EOFError:
+					break
+
+				if data is None:
+					self.stop()
+					break
+				key, args, kwargs=data
+
+				try:
+					self._calls[key](*args, **kwargs)
+				except:
+					if suppress_call_errors:
+						import traceback
+						traceback.print_exc()
+					else:
+						raise
+		finally:
+			with self._exec_running_lock:
+				self._exec_running=False
+
+	def start(self)->None:
+		"""
+		Run self.exec_() in another thread.
+
+		It must be called at most once per Message object.
+		"""
+		self._exec_thread.start()
+
+	def stop(self)->None:
+		"""
+		Stop the exec thread of both processes.
+
+		This will only work if both processes have `exec_` running.
+		"""
+		self._connection.send(None)
+
+	@property
+	def call(self)->Any:
+		return self._call_proxy
+
+	@property
+	def func(self)->Any:
+		return self._func_proxy
+
+	def call_remote(self, key: Any, args: tuple, kwargs: dict)->None:
+		self._connection.send((key, args, kwargs))
+
+	def func_remote(self, key: Any, args: tuple, kwargs: dict)->Any:
+		with self._response_counter_lock:
+			response_counter=self._response_counter=self._response_counter+1
+		with self._exec_running_lock:
+			if not self._exec_running:
+				raise RuntimeError("exec_ is not running")
+		assert response_counter not in self._func_response_queues
+		self._func_response_queues[response_counter]=Queue(1)
+		self.call_remote(_FUNCTION, (key, args, kwargs, response_counter), {})
+		result: Any=self._func_response_queues[response_counter].get()
+		del self._func_response_queues[response_counter]
+		return result
+
+	def _on_func_called(self, key: Any, args: list, kwargs: dict, response_counter: int)->None:
+		result=self._funcs[key](*args, **kwargs)
+		self.call_remote(_FUNCTION_RESPONSE, (response_counter, result), {})
+
+	def _on_func_response(self, response_counter: Any, result: Any)->None:
+		self._func_response_queues[response_counter].put(result)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/bin/python
 import sys
-from subprocess_connection import Connection
+from subprocess_connection import Connection, Message
 
 if len(sys.argv)==1:
 	# parent process
@@ -20,6 +20,29 @@ else:
 		connection=Connection()
 		connection.send(1)
 		print("2 ==", connection.recv())
+
+	elif sys.argv[1]=="1":
+		# parent process (test 2 -- Message)
+		import subprocess
+		message=Message(subprocess.Popen([sys.executable, __file__, "2"],
+			stdin=subprocess.PIPE,
+			stdout=subprocess.PIPE,
+			))
+		message.start()
+		print(message.func.sqrt(5))
+		message.stop()
+
+	elif sys.argv[1]=="2":
+		# child process (test 2 -- Message)
+		message=Message()
+		from time import sleep
+		def sqrt(x):
+			print("before sleep")
+			sleep(1)
+			print("after sleep")
+			return x**.5
+		message.func.sqrt=sqrt
+		message.exec_()
 
 	else:
 		assert False
